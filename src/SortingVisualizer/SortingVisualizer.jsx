ba7ac713473b06a6a16a8cd40f6d610dc067c614
Hamza <hamza.seghir@protonmail.com>
2021-06-08 14:46:48 +0200

    Inifinitly generating array

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,45 +1,47 @@
 import { useEffect, useState } from 'react';
-import { render } from 'react-dom';
 import './SortingVisualizer.css';
 import React from 'react';
 
 function SortingVisualizer(){
 
-    let values = useState([]);
-    generateArray(1,500);
-    // Déclare une nouvelle variable d'état, que nous appellerons « count »
-    const [count, setCount] = useState(0);
+    let [values, setValues] = useState([]);
+    //values = generateArray(1,500);
 
-    return (
-    <>  
-        <section className="array-container">
-        {values.map((element, index)=> {
-            return <div 
-                        className="array-value" 
-                        key={index}
-                        style={{ height:`${element}px`}}>       
-                        </div>
-        })}
-        </section>
-        <div>
-            <p>Nouveau Tableau</p>
-            <button onClick={generateArray(1,500)}>
-            Cliquez ici
-            </button>
-        </div>
-    </>
-   
-    );
+    console.log(values);
+
+    useEffect(() => {
+        console.log("render");   
+        setValues(generateArray(1,500));
+    }, [values])
 
     function generateArray(min, max){
         let array = [];
         for(let i = 0; i< 10; i++){
             array.push(generateNumber(min,max))
         }
-        values = array;
-        return values;
+        return array;
     }
-    
+
+    return (
+    <>  
+        <section className="array-container">
+        {values.map((value, index)=> {
+            return <div 
+                        className="array-value" 
+                        key={index}
+                        style={{ height:`${value}px`}}>
+                        {value}       
+                        </div>
+        })}
+        </section>
+        <div>
+            <p>Nouveau Tableau</p>
+            <button onClick={() => values = generateArray(1,500)}>
+            Cliquez ici
+            </button>
+        </div>
+    </> 
+    ); 
 }
 
 
