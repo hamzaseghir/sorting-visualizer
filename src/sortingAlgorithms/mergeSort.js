613f44fc0e80f0a91ea614beaba21d23f67bfff6
Hamza <hamza.seghir@protonmail.com>
2021-06-11 11:25:05 +0200

    clean up merge sort

--- src/sortingAlgorithms/mergeSort.js
+++ src/sortingAlgorithms/mergeSort.js
@@ -1,25 +1,18 @@
 export function mergeSort(array){
     if (array.length === 1) return array;
-    // console.log("array = " + array);
     const half = Math.round(array.length / 2);
     const first = mergeSort(array.slice(0, half));
-    // console.log("first = " + first);
     const second = mergeSort(array.slice(half));
-    // console.log("second = " + second);
 
     const arr = [];
     let i = 0; // first index
     let j = 0; // second index
     let k = 0; // merged array index
     while(i < first.length && j < second.length){
-        if(first[i] < second[j]){
-            arr[k] = first[i];
-            i++;
-        }
-        else{
-            arr[k] = second[j];
-            j++;
-        }
+        if(first[i] < second[j])
+            arr[k] = first[i++];
+        else
+            arr[k] = second[j++];
         k++;
     }
     // Quand un des tableau est épuisé, on push le reste de l'autre sortedArray
