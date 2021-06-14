65fb6139dc0a47d502118c6ed75643ce63caf9df
Hamza <hamza.seghir@protonmail.com>
2021-06-14 10:02:12 +0200

    Merge sort with index

--- src/sortingAlgorithms/mergeSort.js
+++ src/sortingAlgorithms/mergeSort.js
@@ -22,24 +22,41 @@
 //     return arr;
 // }
 
-export function mergeSort(array, startIndex = 0, endIndex){
-    if (array.length <= 1) return array;
-    const half = Math.round(array.length / 2);
-    const first = mergeSort(array.slice(0, half));
-    const second = mergeSort(array.slice(half));
+export function getMergeSortAnim(array){
+    const anim = [];
+    mergeSort(array,0, array.length-1, anim);
+    return anim;
+}
+
+function mergeSort(baseArray, startIdx, endIdx, anim ){
+    if ((startIdx - endIdx) <= 1) return;
+    const half = Math.round((startIdx + endIdx) / 2);
 
     const arr = [];
-    let i = 0; // first index
-    let j = 0; // second index
-    let k = 0; // merged array index
-    while(i < first.length && j < second.length){
-        if(first[i] < second[j])
+    let i = startIdx; // first index
+    let j = half+1; // second index
+    let k = startIdx; // base array index
+
+    while(i < half && j < endIdx){
+        // valeurs qu'on compare
+        anim.push([i,j]);
+        if(first[i] < second[j]){
+            
             arr[k] = first[i++];
-        else
+        }     
+        else{
             arr[k] = second[j++];
+        }
+            
         k++;
     }
-    while(i < first.length) arr.push(first[i++])
-    while(j < second.length) arr.push(second[j++])
+
+    while(i < first.length){
+        arr.push(first[i++]);
+    } 
+
+    while(j < second.length) {
+        arr.push(second[j++]);
+    }
     return arr;
 }
