94f7a0a749a2c8e2814b47874a194803a7ab2907
Hamza <hamza.seghir@protonmail.com>
2021-06-16 10:36:14 +0200

    clean up bubbleSort

--- src/sortingAlgorithms/bubbleSort.js
+++ src/sortingAlgorithms/bubbleSort.js
@@ -1,6 +1,5 @@
 export default function bubbleSort(array){
     const anim = [];
-    const auxArray = [...array];
     for(let i = 0 ; i < array.length ; i++){
         for(let j = 0; j < array.length - i - 1; j++){
             if(array[j] > array[j+1]){
